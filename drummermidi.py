9d3905b202058492b59d96ce32cde32e2698817c
Madmats14 <hd.cuevas@yahoo.com.ar>
2022-06-02 14:51:45 -0300

    first commit

---
+++ drummermidi.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame import mixer
+pygame.init()
+
+WIDTH = 1400
+HEIGHT = 800
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+gray = (128, 128, 128)
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Beat Mixer')
+label_font = pygame.font.Font('freesansbold.tff')
